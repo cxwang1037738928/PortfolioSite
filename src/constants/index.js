6bb55a2367e19826b62cf07ec29a05cb6e36d4c3
ericw <ericwang030@gmail.com>
2025-11-22 16:27:15 -0500

    added Hero.jsx, modified homescreen to have background image and lines of text displayed. Added rotating icons and text

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+// src/constants/index.js
+
+// A file to hold all the constants across the website so
+// they can be easily modifiable and not clogg up space
+
+export const words = [ /* looping words with images that go into the first h1 element */
+    {text: 'Ideas', imgPath: '/images/ideas.svg'},
+    {text: 'Concepts', imgPath: '/images/concepts.svg'}
+]
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { words } from '../constants/index.js'
+
+const Hero = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 z-10">
+            <img src="/images/bg.png" alt="background"/>
+        </div>
+
+        <div className="hero-layout">
+            {/* LEFT: HERO CONTENT */}
+            <header /* adjusts size based on screen size */ 
+            className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                <div className="flex flex-col gap-7">
+                    <div className="hero-text"/* Main headline, consisting of 3 h1 elements*/> 
+                        <h1>Shaping
+                             <span className="slide" /* rotating slide, maps images from imgpath to rounded boxes*/> 
+                                <span className="wrapper">
+                                    {words.map((words)=> (
+                                        <span key={words.text} className="flex items-center md: gap-3 gap-1 pb-2">
+                                        <img 
+                                            src={words.imgPath}
+                                            alt={words.text}
+                                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                        />
+                                        <span /* Animates the words alongside the icons*/>{words.text}</span>                                 
+                                        </span>
+                                    ))}
+                                </span>
+                             </span>
+                        </h1>
+                        <h2>Into Real Projects</h2>
+                        <h3>That Deliver Results</h3>
+                    </div>
+                </div>
+            </header>
+            {/* RIGHT: 3D MODEL */}
+        </div>
+    </section>
+  )
+}
+
+export default Hero
