4bc9dc8d081776092d969a843be2bfd3a950ac41
ericw <ericwang030@gmail.com>
2025-11-23 16:30:59 -0500

    added navigation bar at the top of the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
+import Navbar from './components/Navbar.jsx'
 
 
 const App = () => {
   return (
     <main>
+        <Navbar/>
         <Hero/>
         <ShowcaseSection/>
     </main>
